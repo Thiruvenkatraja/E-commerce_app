2935f0193e28f46226c75a428c283ae42bbfa5f1
intellectoglobal <intellectoglobal@gmail.com>
2023-02-09 01:42:17 +0530

    added rest_api_framework

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    ProductName = models.CharField(max_length=25)
+    Price = models.DecimalField(max_digits=6, decimal_places=2)
+    Description = models.TextField()
+    Image = models.ImageField(upload_to='products/')
+
+    def __str__(self):
+        return self.ProductName
+    
+
+class Order(models.Model):
+    CustomerName = models.CharField(max_length=25)
+    CustomerEmail = models.EmailField()
+    Product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    Date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.CustomerName} - {self.Product}"
+
