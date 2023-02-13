7e3d288446ca557c06f38827cdc535d8681ae26f
intellectoglobal <intellectoglobal@gmail.com>
2023-02-14 03:37:04 +0530

    Added update and delete in api app

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,5 +3,11 @@ from .views import ProductView, OrderView
 urlpatterns = [
     path("product", ProductView.as_view()),
     path("order", OrderView.as_view()),
+    path("product/<int:id>/update", ProductView.as_view()),
+    path("product/<int:id>/delete", ProductView.as_view()),
+    path("order/<int:id>/update", OrderView.as_view()),
+    path("order/<int:id>/delete", OrderView.as_view()),
+
+
 
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,16 +2,19 @@ from rest_framework.response import Response
 from .serializers import ProductSerializer, OrderSerializer
 from .models import Product, Order
 from rest_framework.views import APIView
-from django.http import JsonResponse
 # Create your views here.
 
 
 class ProductView (APIView):
     serializer_class = ProductSerializer
 
-    def get(self, request, *args, **kwargs):
-        data = Product.objects.all()
-        serializer = ProductSerializer(data, many=True)
+    def get(self, request, id=None, *args, **kwargs):
+        if id:
+            data = Product.objects.get(id=id)
+            serializer = ProductSerializer(data)
+        else:
+            data = Product.objects.all()
+            serializer = ProductSerializer(data, many=True)
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
@@ -19,16 +22,33 @@ class ProductView (APIView):
         serializer = ProductSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-        return Response({"Response": "Data is Successfully saved", "data":serializer.data})
+        return Response({"Response": "Data is Successfully saved", "data": serializer.data})
+
+    def put(self, request, id, *args, **kwargs):
+        data = request.data
+        ProductData = Product.objects.get(id=id)
+        serializer = ProductSerializer(instance=ProductData, data=data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response({"Response": "Data is Successfully Updated", "data": serializer.data})
+
+    def delete(self, request, id, *args, **kwargs):
+        ProductData = Product.objects.filter(id=id)
+        ProductData.delete()
+        return Response({"Response": "Data is Successfully Deleted"})
 
 
 class OrderView (APIView):
     serializer_class = OrderSerializer
 
-    def get(self, request, *args, **kwargs):
-        data = Order.objects.all()
-        serializer = OrderSerializer(data, many=True)
-        return Response(data)
+    def get(self, request, id=None, *args, **kwargs):
+        if id:
+            data = Order.objects.get(id=id)
+            serializer = OrderSerializer(data)
+        else:
+            data = Order.objects.all()
+            serializer = OrderSerializer(data, many=True)
+        return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
         data = request.data
@@ -36,3 +56,17 @@ class OrderView (APIView):
         if serializer.is_valid():
             serializer.save()
         return Response({"Response": "Data is Successfully saved", "data": serializer.data})
+
+    def put(self, request, id, *args, **kwargs):
+        data = request.data
+        OrderData = Order.objects.get(id=id)
+        serializer = OrderSerializer(instance=OrderData, data=data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response({"Response": "Data is Successfully Updated", "data": serializer.data})
+
+
+    def delete(self, request, id, *args, **kwargs):
+        OrderData = Order.objects.filter(id=id)
+        OrderData.delete()
+        return Response({"Response": "Data is Successfully Deleted"})
