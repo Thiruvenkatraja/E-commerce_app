733a96ba8ee53863397f495899ab50ec2df928b1
mohammedadamdev <110044593+mohammedadamdev@users.noreply.github.com>
2023-04-06 02:47:52 +0530

    Added user fields in account app

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models her
+# Create your models here
+
+
+class User(AbstractUser):
+    CreatedAt = models.DateTimeField(auto_now_add=True,  blank=False, null=False)
+    UpdatedAt = models.DateTimeField(auto_now=True, blank=False, null=False)
