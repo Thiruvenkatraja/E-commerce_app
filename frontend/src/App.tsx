3699c963a903c7af7a7d6e45379cac675dfa6abb
RAHUL R <rahulsquads@gmail.com>
2023-04-05 22:01:04 +0530

    ui changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Home } from "./components/Home";
-import  Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-          <Home />
-          {/* <Products/> */}
+      <Home />
+      {/* <Products/> */}
     </div>
   );
 }
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,11 +1,33 @@
-import { Height } from "@mui/icons-material";
+import React from "react";
+
+import Box from "@mui/material/Box";
+import { navLinks } from "../constants";
+import { Carousel } from "antd";
 import Products from "./Products";
 export const Home = () => {
+  const contentStyle: React.CSSProperties = {
+    height: "25rem",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  };
   return (
-    <div style={{ height: "50vh" }}>
-      <div
-        style={{ backgroundColor: "white", width: "50rm", height:"25rem" }}
-      ></div>
+    <div>
+      <Carousel autoplay>
+        <div>
+          <h1 style={contentStyle}>Page 1</h1>
+        </div>
+        <div>
+          <h1 style={contentStyle}>Page 2</h1>
+        </div>
+        <div>
+          <h1 style={contentStyle}>Page 3</h1>
+        </div>
+        <div>
+          <h1 style={contentStyle}>Page 4</h1>
+        </div>
+      </Carousel>
       <Products />
     </div>
   );
