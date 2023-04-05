3557d0374d2c39519949b9cddeae4474ddaa0b81
Manoj <666rmanoj@gmail.com>
2023-04-05 21:41:32 +0530

    changed primary key

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CartView
 
 urlpatterns =[
-    path("cart/<int:id>", CartView.as_view()),
+    path("cart/", CartView.as_view()),
     path("cart/<int:id>/update", CartView.as_view()),
     path("cart/<int:id>/delete", CartView.as_view())
 ]
--- backend/product_and_category/models.py
+++ backend/product_and_category/models.py
@@ -6,7 +6,6 @@ from django.db import models
 
 class Category(models.Model):
     Category = models.CharField(max_length=20)
-    CategoryId = models.CharField(max_length=10, primary_key=True)
 
     class Meta:
         verbose_name_plural = "Category"
@@ -17,12 +16,11 @@ class Category(models.Model):
 class Product(models.Model):
     ProductName = models.CharField(max_length=20)
     Price = models.DecimalField(max_digits=7,decimal_places=2)
-    ProductId = models.CharField(max_length=10, primary_key=True)
     Category = models.ForeignKey(Category, on_delete=models.PROTECT)
     ImageURL = models.ImageField(upload_to='products/', default=None)
 
     def __str__(self):
-        return f"{self.ProductId}"
+        return f"{self.ProductName}"
 
 
 
