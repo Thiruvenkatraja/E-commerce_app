e605e5e49e83543adc0b94fb2aead5261a9a90af
mohammedadamdev <110044593+mohammedadamdev@users.noreply.github.com>
2023-04-12 00:05:01 +0530

    minor modification in cart urls

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CartView
 
 urlpatterns =[
-    path("cart/", CartView.as_view()),
-    path("cart/<int:id>/update", CartView.as_view()),
-    path("cart/<int:id>/delete", CartView.as_view())
+    path("", CartView.as_view()),
+    path("<int:id>/update", CartView.as_view()),
+    path("<int:id>/delete", CartView.as_view())
 ]
